704362ea2cd6ffae37d13bef7513f49b8c5e29e7
sk8erlucas <galileo.laino@gmail.com>
2023-11-06 15:10:11 -0300

    Add header and footer to layout, and update fonts

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,3 @@
+export default function CustomerPage() {
+    return <p>Customers page</p>
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import { montserrat } from "./ui/fonts";
 import "./ui/global.css";
 
@@ -8,7 +9,26 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`${montserrat.className} antialiased`}>{children}</body>
+      <body className={`${montserrat.className} antialiased`}>
+        <header className="flex justify-between bg-blue-500 p-5 text-white">
+          <ul className="flex gap-4">
+            <li>
+              <Link href="/">Home</Link>
+            </li>
+            <li>
+              <Link href="/dashboard">Dashboard</Link>
+            </li>
+            <li>Blog</li>
+            <li>Contact</li>
+          </ul>
+        </header>
+
+        {children}
+
+        <footer className="flex justify-center bg-blue-500 p-5 text-white">
+          <p>Made with love by vercel 💘 </p>
+        </footer>
+      </body>
     </html>
   );
 }

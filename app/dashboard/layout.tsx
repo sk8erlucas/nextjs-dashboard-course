638af9d7590d08104f18b9a5d991656239021e96
sk8erlucas <galileo.laino@gmail.com>
2023-11-07 08:45:39 -0300

    Refactor dashboard layout and navigation links

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,22 +1,17 @@
 import Link from "next/link";
+import SideNav from "../ui/dashboard/sidenav";
 
 export default function DashboardLayout({
-    children,
-  }: {
-    children: React.ReactNode;
-  }) {
-    return (
-      <div className="flex h-screen bg-gray-200">
-        <div className="p-4 space-y-4 bg-blue-500 w-64 h-full text-white">
-          <h1 className="text-2xl font-bold">Dashboard</h1>
-          <nav>
-            <ul className="space-y-2">
-              <li><Link href="/dashboard/customers">Customers</Link></li>
-              <li><Link href="/dashboard/invoices">Invoices</Link></li>
-            </ul>
-          </nav>
-        </div>
-        <div className="flex-grow p-4 overflow-auto">{children}</div>
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div className="flex h-screen">
+      <div className="w-64 h-full bg-blue-500 text-black">
+        <SideNav />
       </div>
-    );
-  }
+      <div className="flex-grow p-4 overflow-auto">{children}</div>
+    </div>
+  );
+}
